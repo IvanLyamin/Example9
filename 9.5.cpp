ec83244e41ba059a404dd6aad314fac19bfd6b09
IvanLyamin <96497489+IvanLyamin@users.noreply.github.com>
2021-12-22 03:47:21 +0300

    Initial commit

---
+++ 9.5.cpp
@@ -0,0 +1,11 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+	int Year = 1500;// Ввод года
+	int Cent = ((Year - 1) / 100) + 1;//Нахождение соответствующего Века
+	printf_s("%i", Cent);
+	return  0;
+}
